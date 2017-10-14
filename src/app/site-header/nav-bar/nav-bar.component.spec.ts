7b699393caf30be6a4a31881f0737129f08d4ddc
ejz22 <ezaremski@gmail.com>
2017-10-14 19:36:01 -0400

    Added Nav Bar 10-13-17

--- src/app/site-header/nav-bar/nav-bar.component.spec.ts
+++ src/app/site-header/nav-bar/nav-bar.component.spec.ts
@@ -22,4 +22,15 @@ describe('NavBarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+var navList = angular.module('navList', []);
+
+navList.controller('navCtrl', ['$scope', '$location', function ($scope, $location) {
+    $scope.navClass = function (page) {
+        var currentRoute = $location.path().substring(1) || 'home';
+        return page === currentRoute ? 'active' : '';
+    };        
+}]);
+
 });
+
+
